75ee503e9a20064bfcb446ef578d9a4f9b1bd7b4
Artem Ivakin <artline17@icloud.com>
2020-01-29 15:01:56 +0300

    Initial Commit

---
+++ SvetoforAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SvetoforAI
+//
+//  Created by Артем Ивакин on 29.01.2020.
+//  Copyright © 2020 artline17@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
